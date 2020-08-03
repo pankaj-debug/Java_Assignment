2bb12e02f789c7f3952b14290fd4231c4cd84663
pankajarya <pankajarya@192.168.43.21>
2020-08-03 09:31:09 +0530

    db properties change

--- Java_Assignment/src/main/java/com/example/demo/Controller/CreatePostController.java
+++ Java_Assignment/src/main/java/com/example/demo/Controller/CreatePostController.java
@@ -1,6 +1,7 @@
 package com.example.demo.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,8 @@ import com.example.demo.Service.CreatePost;
 public class CreatePostController {
 	@Autowired
 	private CreatePost createPost;
+	@Qualifier
+	private Posts posts;
 	@PostMapping(value = "/createPost")
 	public void createPost(@RequestBody Posts posts) {
 		createPost.save(posts);
--- Java_Assignment/src/main/java/com/example/demo/Service/CreatePostImpl.java
+++ Java_Assignment/src/main/java/com/example/demo/Service/CreatePostImpl.java
@@ -1,6 +1,7 @@
 package com.example.demo.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import com.example.demo.Model.Posts;
 import com.example.demo.UserRepository.CreateRepository;
@@ -8,6 +9,9 @@ import com.example.demo.UserRepository.CreateRepository;
 public class CreatePostImpl implements CreatePost{
 	@Autowired
     private CreateRepository createRepository;
+	
+	@Qualifier
+	private Posts posts;
 		
 	public Posts save(Posts posts) {
 		return createRepository.save(posts);
